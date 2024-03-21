af40f16c334c3f1e73b61f1743bc378c913a99cd
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-21 10:50:13 +0000

    Add GAME_BOARD IIFE
    Add GET_BOARD method
    Add UPDATE_BOARD method

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-test
+//const ticTacToe = (function () {
+//	const p1 = 'X';
+//	const p2 = '0';
+//
+//	return {p1};
+//})();
+
+const GAME_BOARD = (function () {
+	const BOARD = [
+		['*', '*', '*'],
+		['*', '*', '*'],
+		['*', '*', '*']
+	];
+	const GET_BOARD = () => {
+		console.log(BOARD);
+	};
+
+	const UPDATE_BOARD = (row, col, player) => {
+		BOARD[row].splice(col, 1, player);
+	};
+
+	return {GET_BOARD, UPDATE_BOARD};
+})();
+
+
