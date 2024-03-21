1d463ef970594bb4e7d9691f8e9634171faae131
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-21 12:30:54 +0000

    Add method CHECK_POS_AVAIL

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,10 @@ const GAME_BOARD = (function () {
 		BOARD[row].splice(col, 1, player);
 	};
 
-	return {GET_BOARD, UPDATE_BOARD};
+	const CHECK_POS_AVAIL = (row, col, player) => {
+		BOARD[row][col] === "*" ? UPDATE_BOARD(row, col, player) : console.log('This square is already in use, pick again');
+	}
+	return {GET_BOARD, CHECK_POS_AVAIL};
 })();
 
-
+console.log('end');
