13e5a03bdffd2da859f12075556b9d3fa62353d7
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-25 15:01:03 +0000

    Add PLAYER and GAME factory functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,61 @@
-//const ticTacToe = (function () {
-//	const p1 = 'X';
-//	const p2 = '0';
-//
-//	return {p1};
-//})();
+const TIC_TAC_TOE = (function () {
 
-const GAME_BOARD = (function () {
-	const BOARD = [
-		['*', '*', '*'],
-		['*', '*', '*'],
-		['*', '*', '*']
-	];
-	const GET_BOARD = () => {
-		console.log(BOARD);
-	};
+	const GAME_BOARD = (function () {
+		const _BOARD = [
+			['*', '*', '*'],
+			['*', '*', '*'],
+			['*', '*', '*']
+		];
+		const GET_BOARD = () => {
+			console.log(_BOARD);
+		};
 
-	const UPDATE_BOARD = (row, col, player) => {
-		BOARD[row].splice(col, 1, player);
-	};
+		const _UPDATE_BOARD = (row, col, player) => {
+			_BOARD[row].splice(col, 1, player);
+		};
 
-	const CHECK_POS_AVAIL = (row, col, player) => {
-		BOARD[row][col] === "*" ? UPDATE_BOARD(row, col, player) : console.log('This square is already in use, pick again');
-	}
-	return {GET_BOARD, CHECK_POS_AVAIL};
+		const CHECK_POS_AVAIL = (row, col, player) => {
+			_BOARD[row][col] === "*" ? _UPDATE_BOARD(row, col, PLAYER._PLAYERS[player].symbol) : console.log('This square is already in use, pick again');
+		}
+		return {GET_BOARD, CHECK_POS_AVAIL};
+	})();
+
+	const PLAYER = (function () {
+			const _PLAYERS = []
+			const CREATE_PLAYER = () => {
+				name = prompt('Enter your name.');
+				const _PLAYER_1 = {name, player: 'Player1', symbol: 'X', score: 0}
+				const _PLAYER_2 = {name, player: 'Player2', symbol: '0', score: 0}
+
+				_PLAYERS[0] === undefined ? _PLAYERS[0] = _PLAYER_1 : _PLAYERS[1] = _PLAYER_2;
+				console.log(_PLAYERS);
+			}
+			const INCREMENT_SCORE = (playerNumber) => {
+				_PLAYERS[playerNumber].score += 1;
+				console.log(_PLAYERS);
+			}
+			return {CREATE_PLAYER, INCREMENT_SCORE, _PLAYERS}
+		}
+	)
+	();
+
+	const GAME = (function () {
+
+		const MAKE_MOVE = (row, col, player) => {
+			GAME_BOARD.CHECK_POS_AVAIL(row, col, player)
+			console.log(GAME_BOARD.GET_BOARD())
+		}
+		return {MAKE_MOVE}
+	})();
+	return {GAME_BOARD, GAME, PLAYER}
 })();
+
+//TODO create move counter that only increments after a valid move
+//TODO select P1 or P2 for next move based on move counter odd vs even values
+//TODO create method for win condition check
+//TODO link to DOM
+//TODO create method for rendering board
+//TODO make methods private where appropriate
+
 
 console.log('end');
