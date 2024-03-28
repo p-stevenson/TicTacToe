ba90e13aa3dae894342b6ff3ce000df6aeb7ddd2
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-03-28 15:50:52 +0000

    UPDATE methods returned by TIC_TAC_TOE

--- scripts/main.js
+++ scripts/main.js
@@ -66,7 +66,8 @@ const TIC_TAC_TOE = (function () {
 
 		return {MAKE_MOVE, DISPLAY_MOVE_COUNTER, _INCREMENT_MOVE_COUNTER, CHECK_FOR_TIE, CHECK_POS_AVAIL};
 	})();
-	return {GAME_BOARD, GAME, PLAYER}
+
+	return {GAME_BOARD, GAME, PLAYER};
 })();
 
 //TODO create move counter that only increments after a valid move
