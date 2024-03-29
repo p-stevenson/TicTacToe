b412a1ecbc9c146c960a43111bc5c15e140aeedc
Peter Stevenson <54287958+p-stevenson@users.noreply.github.com>
2024-03-29 13:31:31 +0000

    Update formatting

--- scripts/main.js
+++ scripts/main.js
@@ -40,18 +40,11 @@ const TIC_TAC_TOE = (function () {
     const GAME = (function () {
         let _move_counter = 0;
 
-        const _INCREMENT_MOVE_COUNTER = () => {
-            _move_counter += 1;
-            console.log(_move_counter);
-        }
+
 
         const MAKE_MOVE = (row, col, index) => {
             _move_counter % 2 === 0 ? index = 0 : index = 1;
             _CHECK_FOR_TIE(row, col, index)
-        }
-
-        const _CHECK_FOR_TIE = (row, col, index) => {
-            _move_counter !== 2 ? _CHECK_POS_AVAIL(row, col, index) : alert("It's a tie");
         }
 
         const _CHECK_POS_AVAIL = (row, col, index) => {
@@ -62,6 +55,14 @@ const TIC_TAC_TOE = (function () {
             } else {
                 console.log('This square is already in use, pick again.');
             }
+        }
+        const _CHECK_FOR_TIE = (row, col, index) => {
+            _move_counter !== 2 ? _CHECK_POS_AVAIL(row, col, index) : alert("It's a tie");
+        }
+
+        const _INCREMENT_MOVE_COUNTER = () => {
+            _move_counter += 1;
+            console.log(_move_counter);
         }
 
         return {MAKE_MOVE};
