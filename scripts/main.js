2fe378f6275840eb06634026ce290fe9af9c162b
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2024-05-06 14:32:28 +0100

    Update naming convention and variable declarations

--- scripts/main.js
+++ scripts/main.js
@@ -1,162 +1,162 @@
 const TIC_TAC_TOE = (function () {
 
-    const _PLAYERS_LIST = [];
+	let _playerList = [];
 
-    //Cache DOM
-    const P1_BUTTON = document.getElementById("P1_BUTTON");
-    const P2_BUTTON = document.getElementById("P2_BUTTON");
-    const BOARD_SQUARES = document.querySelectorAll('.square');
-    const RESET_BUTTON = document.getElementById("RESET_BUTTON");
+	//Cache DOM
+	let p1Button = document.getElementById("p1Button");
+	let p2Button = document.getElementById("p2Button");
+	let boardSquares = document.querySelectorAll('.square');
+	let resetButton = document.getElementById("resetButton");
 
-    //Bind Events
-    P1_BUTTON.addEventListener('click', () => {
-        PLAYER.ADD_PLAYER(1);
-    });
+	//Bind Events
+	p1Button.addEventListener('click', () => {
+		PLAYER.addPlayer(1);
+	});
 
-    P2_BUTTON.addEventListener('click', () => {
-        PLAYER.ADD_PLAYER(2);
-    });
+	p2Button.addEventListener('click', () => {
+		PLAYER.addPlayer(2);
+	});
 
-    BOARD_SQUARES.forEach((item) => {
-        item.addEventListener('click', () => {
-            GAME.MAKE_MOVE(item.getAttribute('data-row'), item.getAttribute('data-column'));
-        });
-    });
+	boardSquares.forEach((item) => {
+		item.addEventListener('click', () => {
+			GAME.makeMove(item.getAttribute('data-row'), item.getAttribute('data-column'));
+		});
+	});
 
-    RESET_BUTTON.addEventListener('click', () => {
-        location.reload();
-    });
+	resetButton.addEventListener('click', () => {
+		location.reload();
+	});
 
-    const GAME_BOARD = (function () {
-        const BOARD = [
-            [' ', ' ', ' '],
-            [' ', ' ', ' '],
-            [' ', ' ', ' ']
-        ];
+	const GAME_BOARD = (function () {
+		let board = [
+			[' ', ' ', ' '],
+			[' ', ' ', ' '],
+			[' ', ' ', ' ']
+		];
 
-        const UPDATE_BOARD = (row, col, playerSymbol) => {
-			BOARD[row].splice(col, 1, playerSymbol);
-        };
+		let updateBoard = (row, col, playerSymbol) => {
+			board[row].splice(col, 1, playerSymbol);
+		};
 
-        return {BOARD, UPDATE_BOARD};
-    })();
+		return {board, updateBoard};
+	})();
 
-    const PLAYER = (function () {
-        const _CREATE_PLAYER = (playerNumber) => {
-            const name = prompt('Enter your name.');
-            const playerSymbol = playerNumber === 1 ? 'X' : 'O';
-            const score = 0;
-            return {name, playerNumber, playerSymbol, score};
-        };
+	const PLAYER = (function () {
+		let _createPlayer = (playerNumber) => {
+			let name = prompt('Enter your name.');
+			let playerSymbol = playerNumber === 1 ? 'X' : 'O';
+			let score = 0;
+			return {name, playerNumber, playerSymbol, score};
+		};
 
-        const ADD_PLAYER = (playerNumber) => {
-            _PLAYERS_LIST.push(_CREATE_PLAYER(playerNumber))
-        };
+		let addPlayer = (playerNumber) => {
+			_playerList.push(_createPlayer(playerNumber))
+		};
 
-        return {ADD_PLAYER};
-    })();
+		return {addPlayer};
+	})();
 
-    const GAME = (function () {
-        let _move_counter = 0;
+	const GAME = (function () {
+		let _moveCounter = 0;
 
-        const _P1_WIN_STATES = [
-            // Rows
-            [['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' ']],
-            [[' ', ' ', ' '], ['X', 'X', 'X'], [' ', ' ', ' ']],
-            [[' ', ' ', ' '], [' ', ' ', ' '], ['X', 'X', 'X']],
+		let _p1WinState = [
+			// Rows
+			[['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' ']],
+			[[' ', ' ', ' '], ['X', 'X', 'X'], [' ', ' ', ' ']],
+			[[' ', ' ', ' '], [' ', ' ', ' '], ['X', 'X', 'X']],
 
-            // Columns
-            [['X', ' ', ' '], ['X', ' ', ' '], ['X', ' ', ' ']],
-            [[' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' ']],
-            [[' ', ' ', 'X'], [' ', ' ', 'X'], [' ', ' ', 'X']],
+			// Columns
+			[['X', ' ', ' '], ['X', ' ', ' '], ['X', ' ', ' ']],
+			[[' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' ']],
+			[[' ', ' ', 'X'], [' ', ' ', 'X'], [' ', ' ', 'X']],
 
-            // Diagonals
-            [['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X']],
-            [[' ', ' ', 'X'], [' ', 'X', ' '], ['X', ' ', ' ']]
-        ];
+			// Diagonals
+			[['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X']],
+			[[' ', ' ', 'X'], [' ', 'X', ' '], ['X', ' ', ' ']]
+		];
 
-        const _P2_WIN_STATES = [
-            // Rows
-            [['O', 'O', 'O'], [' ', ' ', ' '], [' ', ' ', ' ']],
-            [[' ', ' ', ' '], ['O', 'O', 'O'], [' ', ' ', ' ']],
-            [[' ', ' ', ' '], [' ', ' ', ' '], ['O', 'O', 'O']],
+		let _p2WinState = [
+			// Rows
+			[['O', 'O', 'O'], [' ', ' ', ' '], [' ', ' ', ' ']],
+			[[' ', ' ', ' '], ['O', 'O', 'O'], [' ', ' ', ' ']],
+			[[' ', ' ', ' '], [' ', ' ', ' '], ['O', 'O', 'O']],
 
-            // Columns
-            [['O', ' ', ' '], ['O', ' ', ' '], ['O', ' ', ' ']],
-            [[' ', 'O', ' '], [' ', 'O', ' '], [' ', 'O', ' ']],
-            [[' ', ' ', 'O'], [' ', ' ', 'O'], [' ', ' ', 'O']],
+			// Columns
+			[['O', ' ', ' '], ['O', ' ', ' '], ['O', ' ', ' ']],
+			[[' ', 'O', ' '], [' ', 'O', ' '], [' ', 'O', ' ']],
+			[[' ', ' ', 'O'], [' ', ' ', 'O'], [' ', ' ', 'O']],
 
-            // Diagonals
-            [['O', ' ', ' '], [' ', 'O', ' '], [' ', ' ', 'O']],
-            [[' ', ' ', 'O'], [' ', 'O', ' '], ['O', ' ', ' ']]
-        ];
+			// Diagonals
+			[['O', ' ', ' '], [' ', 'O', ' '], [' ', ' ', 'O']],
+			[[' ', ' ', 'O'], [' ', 'O', ' '], ['O', ' ', ' ']]
+		];
 
-        const MAKE_MOVE = (row, col) => {
-            let index;
-            _move_counter % 2 === 0 ? index = 0 : index = 1;
-            _CHECK_POS_AVAIL(row, col, index) ? _CHECK_FOR_WIN() : _CHECK_FOR_TIE();
-        };
+		let makeMove = (row, col) => {
+			let index;
+			_moveCounter % 2 === 0 ? index = 0 : index = 1;
+			_checkPosAvail(row, col, index) ? _checkWin() : _checkTie();
+		};
 
-        const _CHECK_POS_AVAIL = (row, col, index) => {
-            if (GAME_BOARD.BOARD[row][col] === ' ') {
-                GAME_BOARD.UPDATE_BOARD(row, col, _PLAYERS_LIST[index].playerSymbol);
-                _RENDER();
-                return true
-            } else {
-                console.log('This square is already in use, pick again.');
-                return false
-            }
-        };
-        const _CHECK_FOR_TIE = () => {
-            if (_move_counter === 9) alert("It's a tie");
-        };
+		let _checkPosAvail = (row, col, index) => {
+			if (GAME_BOARD.board[row][col] === ' ') {
+				GAME_BOARD.updateBoard(row, col, _playerList[index].playerSymbol);
+				_render();
+				return true
+			} else {
+				console.log('This square is already in use, pick again.');
+				return false
+			}
+		};
+		let _checkTie = () => {
+			if (_moveCounter === 9) alert("It's a tie");
+		};
 
-        const _CHECK_FOR_WIN = () => {
-            let _sanatizedBoard;
-            _INCREMENT_MOVE_COUNTER();
-            if ((_move_counter >= 5) && (_move_counter % 2 !== 0)) {
-                _sanatizedBoard = JSON.stringify(GAME_BOARD.BOARD).replaceAll(/(?!X)[A-Z]/gm, ' ');
-                _CHECK_P1_WIN_STATES(_sanatizedBoard);
-            } else if ((_move_counter >= 5) && (_move_counter % 2 === 0)) {
-                _sanatizedBoard = JSON.stringify(GAME_BOARD.BOARD).replaceAll(/(?!O)[A-Z]/gm, ' ');
-                _CHECK_P2_WIN_STATES(_sanatizedBoard);
-            }
-        };
+		let _checkWin = () => {
+			let _sanatizedBoard;
+			_incrementMoveCounter();
+			if ((_moveCounter >= 5) && (_moveCounter % 2 !== 0)) {
+				_sanatizedBoard = JSON.stringify(GAME_BOARD.board).replaceAll(/(?!X)[A-Z]/gm, ' ');
+				_checkP1Win(_sanatizedBoard);
+			} else if ((_moveCounter >= 5) && (_moveCounter % 2 === 0)) {
+				_sanatizedBoard = JSON.stringify(GAME_BOARD.board).replaceAll(/(?!O)[A-Z]/gm, ' ');
+				_checkP2Win(_sanatizedBoard);
+			}
+		};
 
-        const _CHECK_P1_WIN_STATES = (_sanatizedBoard) => {
-            _P1_WIN_STATES.forEach((element) => {
-                if (JSON.stringify(element) === _sanatizedBoard) {
-                    return alert(`P1 Wins`);
-                }
-            });
-        };
+		let _checkP1Win = (_sanatizedBoard) => {
+			_p1WinState.forEach((element) => {
+				if (JSON.stringify(element) === _sanatizedBoard) {
+					return alert(`P1 Wins`);
+				}
+			});
+		};
 
-        const _CHECK_P2_WIN_STATES = (_sanatizedBoard) => {
-            _P2_WIN_STATES.forEach((element) => {
-                if (JSON.stringify(element) === _sanatizedBoard) {
-                    return alert(`P2 Wins`);
-                }
-            });
-        };
+		let _checkP2Win = (_sanatizedBoard) => {
+			_p2WinState.forEach((element) => {
+				if (JSON.stringify(element) === _sanatizedBoard) {
+					return alert(`P2 Wins`);
+				}
+			});
+		};
 
-        const _INCREMENT_MOVE_COUNTER = () => {
-            _move_counter += 1;
-        };
+		let _incrementMoveCounter = () => {
+			_moveCounter += 1;
+		};
 
-        return {MAKE_MOVE};
-    })();
+		return {makeMove};
+	})();
 
-    const _RENDER = () => {
-        let counter = 0
-        GAME_BOARD.BOARD.forEach((row) => {
-            row.forEach((element) => {
-                document.getElementById(`#${counter}`).textContent = element;
-                counter += 1;
-            });
-        });
-    };
-    _RENDER()
-    return {GAME, PLAYER};
+	let _render = () => {
+		let counter = 0
+		GAME_BOARD.board.forEach((row) => {
+			row.forEach((element) => {
+				document.getElementById(`#${counter}`).textContent = element;
+				counter += 1;
+			});
+		});
+	};
+	_render()
+	return {GAME, PLAYER};
 })();
 
 console.log('end');
